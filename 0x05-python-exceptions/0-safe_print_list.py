0964831c48b02413f4a09d45723f14157f32077b
Gideon <gids2all@gmail.com>
2023-10-23 08:31:39 -0700

    print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     nums = 0
-   for i in range(x):
-        try: print("{}".format(my_lists[i], end="")
+    for i in range(x):
+        try: print("{}".format(my_lists[i], end=""))
                 nums += 1
                 except IndexError:
                 break
