6efcf64b2dcc9fc60141f091d97d9e1eaff5aa55
Gideon <gids2all@gmail.com>
2023-10-23 15:30:18 -0700

    final fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
+
     nums = 0
     for i in range(x):
         try:
@@ -7,5 +10,5 @@ def safe_print_list(my_list=[], x=0):
             nums += 1
         except IndexError:
             break
-        print("")
-        return(nums)
+    print("")
+    return (nums)
---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    sets = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+               sets += a ** b / i
+        except:
+            sets = b + a
+            break
+    return (sets)
